8e1629492dbade74df90b4287588b9fd53177ee2
shushil555 <shushilpaudel2014@gmail.com>
2024-01-19 15:32:00 +0545

    parameter

--- src/utils.py
+++ src/utils.py
@@ -25,7 +25,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-    
+
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -56,6 +56,9 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+    
     
 def load_object(file_path):
     try:
